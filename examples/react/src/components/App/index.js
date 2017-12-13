4580bf76510a756c6333a98dd465bcf378d6d8dc
Taras Mankovski <tarasm@gmail.com>
2017-12-13 17:05:37 -0500

    Upgraded to use microstates-todomvc package

--- examples/react/src/components/App/index.js
+++ examples/react/src/components/App/index.js
@@ -1,5 +1,5 @@
 import connect from '../../utils/connect'
 import Component from './component'
-import Model from 'microstates-todomvc-model'
+import Model from 'microstates-todomvc'
 
 export default connect(Model, Component)
