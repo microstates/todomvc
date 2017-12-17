4b06487c92af56b90a481f521f8053f5281b52c5
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-12-17 20:34:34 +0530

    Add routing using navigo

--- examples/glimmer/src/ui/components/TodoApp/component.ts
+++ examples/glimmer/src/ui/components/TodoApp/component.ts
@@ -1,4 +1,21 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
+import Navigo from 'navigo';
+
+const router = new Navigo(null, true);
 
 export default class TodoApp extends Component {
+  @tracked mode: string = 'all';
+
+  constructor(options) {
+    super(options);
+
+    router
+    .on({
+      '/': () => { this.mode = 'all'; },
+      '/active': () => { this.mode = 'active'; },
+      '/completed': () => { this.mode = 'completed'; },
+    })
+    .resolve();
+  }
+
 }
