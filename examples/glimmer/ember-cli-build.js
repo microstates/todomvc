4993f9dd3375f78a6cd477b259452bd51c88afc1
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-12-17 20:03:01 +0530

    Add the todomvc microstate & configure ember-cli-build

--- examples/glimmer/ember-cli-build.js
+++ examples/glimmer/ember-cli-build.js
@@ -1,10 +1,21 @@
 'use strict';
 
 const GlimmerApp = require('@glimmer/application-pipeline').GlimmerApp;
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
 
 module.exports = function(defaults) {
   let app = new GlimmerApp(defaults, {
-    // Add options here
+    rollup: {
+      plugins: [
+        resolve({ jsnext: true, module: true, main: true }),
+        commonjs({
+          namedExports: {
+            'node_modules/ramda/index.js': Object.keys(require('ramda'))
+          }
+        })
+      ]
+    }
   });
 
   return app.toTree();
