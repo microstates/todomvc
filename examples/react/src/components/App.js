bee9602e7e95b975a5ddf21ade710fc72fc40abc
Taras Mankovski <tarasm@gmail.com>
2017-12-24 10:18:53 -0500

    Simplified TodoMVC example to eliminate extending the model

--- examples/react/src/components/App.js
+++ examples/react/src/components/App.js
@@ -1,33 +1,9 @@
 import React from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
-import qs from 'qs'
 
 import Model from 'microstates-todomvc'
-import * as MS from 'microstates'
 import connect from '../utils/connect'
 import TodoMVC from './TodoMVC'
-
-class WindowLocation {
-  hash = MS.String
-  pathname = MS.String
-  search = MS.String
-
-  get qs() {
-    return qs.parse(this.search, { ignoreQueryPrefix: true })
-  }
-}
-
-class RoutableTodoMVC extends Model {
-  location = WindowLocation
-
-  get filter() {
-    return this.location.qs.filter || ''
-  }
-
-  set filter(v = '') {
-    return v
-  }
-}
 
 function App({ model, actions, history }) {
   return (
@@ -41,4 +17,4 @@ function App({ model, actions, history }) {
   )
 }
 
-export default connect(RoutableTodoMVC, App)
+export default connect(Model, App)
--- examples/react/src/utils/connect.js
+++ examples/react/src/utils/connect.js
@@ -3,48 +3,77 @@ import { map } from 'funcadelic'
 import hoistStatics from 'hoist-non-react-statics'
 import microstate from 'microstates'
 import createBrowserHistory from 'history/createBrowserHistory'
+import qs from 'qs'
 
+/**
+ * Connect function creates a wrapper around a React Component. This wrapper
+ * manages the microstate and ensuring that invoked transitions re-render the
+ * wrapped component.
+ *
+ * @param {Microstate} Model
+ * @param {ReactComponent} WrappedComponent
+ */
 export default function connect(Model, WrappedComponent) {
   class Connect extends Component {
     constructor(props, context) {
       super(props, context)
 
+      /**
+       * Creating a history object allows us to listen to url changes and
+       * update the filter property when the query parameter changes.
+       */
       this.history = createBrowserHistory()
-      this.setMicrostate(
-        microstate(Model, {
-          ...props,
-          location: this.history.location
-        })
-      )
+
+      /**
+       * Create microstate with initial state. This is what the users will see when
+       * the app first loads.
+       */
+      this.microstate = microstate(Model, {
+        ...props,
+        ...this.queryParams
+      })
     }
 
-    setMicrostate(ms) {
-      this.microstate = ms
+    /**
+     * Extracts query params from History Location
+     */
+    get queryParams() {
+      return qs.parse(this.history.location.search, { ignoreQueryPrefix: true })
+    }
 
-      this.actions = map(
+    /**
+     * Actions are transitions that are wrapped with code that'll cause the component to rerender.
+     */
+    get actions() {
+      return map(
         transition => (...args) => {
-          let ms = transition(...args)
-          this.setMicrostate(ms)
-          this.setState({})
+          this.microstate = transition(...args)
+          this.forceUpdate()
         },
-        ms
+        this.microstate
       )
     }
 
-    historyListener = location => {
-      this.actions.location.set(location)
+    /**
+     * When history changes, update the filter property with value of filter from query params.
+     */
+    historyListener = () => {
+      this.actions.filter.set(this.queryParams.filter)
     }
 
     componentWillMount() {
+      /**
+       * Setup history listener when component is rendered
+       */
       this.unlisten = this.history.listen(this.historyListener)
     }
 
     componentWillUnmount() {
+      /**
+       * Cleanup the listeners
+       */
       this.unlisten()
-    }
-
-    componentWillReceiveProps(props) {
-      this.actions.set(props)
+      this.unlisten = null
     }
 
     render() {
