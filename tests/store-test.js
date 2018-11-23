d66eb9c9e66ce914bec90a92a2cf3a398bd644a3
Taras Mankovski <tarasm@gmail.com>
2018-11-23 14:31:08 -0500

    Added simple store test

---
+++ tests/store-test.js
@@ -0,0 +1,21 @@
+import { create, Store } from "microstates";
+import TodoMVC from "../todomvc";
+
+let state = create(TodoMVC, {
+  todos: [
+    { id: 1, text: "Make initial commit", completed: false },
+    { id: 2, text: "Write readme", completed: false },
+    { id: 3, text: "Release microstates", completed: false }
+  ]
+});
+
+describe('store', () => {
+  let store;
+  beforeEach(() => {
+    store = Store(state, next => store = next);
+  });
+
+  it('has an Identity on store.newTodo', () => {
+    expect(store.newTodo.constructor.name).toBe("Id<String>")
+  });
+});
